30dca47fcd0e2b6cfacc8eaac9a35ea261d3b4eb
AyushUprety <ayushuprety1@gmail.com>
2021-10-03 12:26:40 +0545

    fixed cannot read property of undefined using bind method

--- Imgproject/my-app/src/components/searchbar.js
+++ Imgproject/my-app/src/components/searchbar.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 class Searchbar extends React.Component{
-   onInputChange(event){
-       console.log(event.target.value);
-   }
+  state={text:'Enter the Value:'}
+//   constructor(){
+//       super();
+//       this.onFormSubmit = this.onFormSubmit.bind(this);
+//   }
+  onFormSubmit=(e)=>{ 
+    e.preventDefault();
+    console.log(this.state.text);
+  }
    render() {
     return (
         <div>
-            <form className="ui container form segment" style={{marginTop: '10px'}}>
+            <form onSubmit={this.onFormSubmit} className="ui container form segment" style={{marginTop: '10px'}}>
                 <div className="field">
                 <label for="search">Image Search</label>
-                <input type="text" id="search" name="search" onChange={this.onInputChange}></input>
+                <input type="text" id="search" name="search" value={this.state.text} onChange={e=>this.setState({text:e.target.value})}></input>
                 </div>
                 
             </form>
