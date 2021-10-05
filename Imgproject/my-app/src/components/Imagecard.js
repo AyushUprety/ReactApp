e72609067ed4e6ec3da9c5e1533f63d7d925ad1d
AyushUprety <ayushuprety1@gmail.com>
2021-10-05 12:05:41 +0545

    added event listener in didmount() method to ensure we get clientHeight after the image is loaded

--- Imgproject/my-app/src/components/Imagecard.js
+++ Imgproject/my-app/src/components/Imagecard.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.refer = React.createRef();
+    }
+    componentDidMount() {
+        this.refer.current.addEventListener('load',this.setSpans)
+    }
+    setSpans=() => {
+        console.log(this.refer.current.clientHeight);
+    }
+
+    
     render() {
-        console.log(this.props)
+        // console.log(this.props)
         const{alt,src}=this.props;
         return (
         <div>
             <img 
+                ref={this.refer}
                 alt={alt} 
                 src={src}
             />
