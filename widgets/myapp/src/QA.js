3f8bf8539d8f753fd96c9967f5cef63f47c6b2fc
AyushUprety <ayushuprety1@gmail.com>
2021-10-08 12:22:32 +0545

    added accordon to show the list

--- widgets/myapp/src/QA.js
+++ widgets/myapp/src/QA.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const widget = ()=>{
+const widget = ({Items})=>{
+    const renderesList = Items.map(item =>{
     return (
-        <div>Widget bong </div>
+        <div class="ui styled accordion">
+            <div class="active title">
+                <i class="dropdown icon"></i>
+                    {item.title}
+            </div>
+                <div class="active content">
+                    <p>{item.description}</p>
+            </div>
+       </div> 
+    
+    )
+    })
+    return (
+       <div>{renderesList}</div>
     )
 }
 export default widget;
--- widgets/myapp/src/app.js
+++ widgets/myapp/src/app.js
@@ -3,8 +3,22 @@ import QA from './QA'
 
 const App=()=>
 {
+    const items = [
+        { 
+            title:'What is React?',
+            description:'React is a front end library'
+        },
+        { 
+            title:'Why use React?',
+            description:'React is a favourite js library among engineers'
+        },
+        { 
+            title:'How do you use React',
+            description:'You use react by creating componenets'
+        }
+    ]
     return(
-        <QA/>
+        <QA Items={items}/>
 
     )
 }
