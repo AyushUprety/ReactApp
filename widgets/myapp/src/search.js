f105a90f4702cbd9d1e4bce2ebac8ebd719ccbed
AyushUprety <ayushuprety1@gmail.com>
2021-10-10 12:07:32 +0545

    Successfully fetched response destructured it and updated the useState with that data

--- widgets/myapp/src/search.js
+++ widgets/myapp/src/search.js
@@ -2,28 +2,37 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search =()=>{
-    const[ term, setTerm]=useState('');
+    const[ Term, setTerm]=useState('programming');
+    const[Result, setResult]=useState([]);
+    console.log(Result);
 
    useEffect(()=>{
-       const text = async ()=>{
-           await axios.get('https://en.wikipedia.org//w/api.php',{
+       const search = async ()=>{
+           const {data} = await axios.get('https://en.wikipedia.org//w/api.php',{
                params: {
                     action: 'query',
                     list:'search',
                     origin:'*',
+                    format: 'json',
+                    srsearch:Term,
                     
                }    
            })
+           setResult(data.query.search);
+
+
        }
-       text();
-   },[term])
+       search();
+       console.log(Result);
+    
+   },[Term])
 
     return (
         <div>
-            <form class="ui form">
+            <form class="ui segment container ">
                 <div class="field">
-                    <label>Enter the term to Search:</label>
-                    <input type="text" name="search" onChange={(e)=>{setTerm(e.target.value)}}></input>
+                    <label>Enter the Term to Search:</label>
+                    <input type="data" name="search" onChange={(e)=>{setTerm(e.target.value)}}></input>
                 </div>
             </form>    
         </div>
