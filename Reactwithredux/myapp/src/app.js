906a23f99997b427144ccb4841a33537f6f876d7
AyushUprety <ayushuprety1@gmail.com>
2021-11-18 13:06:37 +0545

    Connected the child compnent Song with the parent component app

--- Reactwithredux/myapp/src/app.js
+++ Reactwithredux/myapp/src/app.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Song from './song';
 
 const App =()=>{
     return (
-        'test'
+            <div>
+                <Song title={ballaballa} length={2.57}/>
+                <Song title={wakkawakka} length={2.30}/>
+                <Song title={perfect} length={5.5}/>
+            </div>
+    
+        
     )
 }
 export default App;
---
+++ Reactwithredux/myapp/src/song.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Song=({title,length})=>{
+    
+    
+    return 'Hello'
+}
+export default Song;
