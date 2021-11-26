3e6823a226dc4cb66cac8c2c4b51ce09c53a353e
AyushUprety <ayushuprety1@gmail.com>
2021-11-26 13:26:15 +0545

    Rendered products usign map function

--- Cart/myapp/src/components/data.js
+++ Cart/myapp/src/components/data.js
@@ -3,11 +3,13 @@ const data = {
         {
             id:1,
             Title:'Nescafe',
+            price:100,
             img:'https://www.okdam.com/public/upload/product/extra/nescafe-gold-coffee-jar-50g-5fe8636a705a3.jpg'
         },
         {
             id:2,
             Title:'CoronaBeer',
+            price:200,
             img:'https://produits.bienmanger.com/34491-0w600h600_Corona_Extra_Mexican_Blonde_Beer.jpg'
         }
     ]
