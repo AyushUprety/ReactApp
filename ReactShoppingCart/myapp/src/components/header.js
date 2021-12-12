4894ee12c6734efdad64db44b71ec73d088addcf
AyushUprety <ayushuprety1@gmail.com>
2021-12-12 06:31:40 +0545

    wrote basic css for navbar component

---
+++ ReactShoppingCart/myapp/src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = ()=>{
+    return(
+        <div class='container'>
+            <h2>Small Shopping Cart</h2>
+            <div className='cart-end'>
+                <h4>Cart</h4>
+                <h4 id="items-amt">3</h4>
+                <h4>Signin</h4>
+            </div>
+           
+        </div>
+    )
+}
+export default Header;
