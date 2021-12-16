0c298a3ad9146e424c2543ad7fdcfa56f59128af
AyushUprety <ayushuprety1@gmail.com>
2021-12-16 06:28:53 +0545

    Added shopping products list

---
+++ ReactShoppingCart/myapp/src/components/data.js
@@ -0,0 +1,18 @@
+const data = [
+    { 
+        id:1,
+        title:'Macbook air',
+        price:2000
+    },
+    {   
+        id:2,
+        title:'Television',
+        price:2000
+    },
+    {   
+        id:3,
+        title:'Mobile',
+        price:2000
+    }
+]
+export default data;
