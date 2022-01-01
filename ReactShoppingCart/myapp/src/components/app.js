8b99ad9289db873ffcbabab2fa7124815c0f0e41
AyushUprety <ayushuprety1@gmail.com>
2022-01-01 08:21:09 +0545

    Added state in app.js to record items in cart

--- ReactShoppingCart/myapp/src/components/app.js
+++ ReactShoppingCart/myapp/src/components/app.js
@@ -1,9 +1,24 @@
+/**The confusion was in line 13. item.id=== le kun sanga compare garnu
+item.id===product.id ma product xi kaha bata launu.I came to conclusion
+that for this to work we will pass product as an argument. So product click
+huda tyo addToCart function run garna which will also take an product as an argument
+jastai tyo product ma products map vayera individual product aako hunxa
+Now that product can be passed as an argument in addToCart function.
+**/
+
 import React from "react";
 import Header from "./header";
 import Product from "./products";
 import Cart from "./cart";
+import {useState} from 'react';
+import Items from './data';
 
 const App = () => {
+  const[cartItems,setCartItems] =useState([]);
+  const addToCart =()=>{
+    // const item = Items.find((item)=>item.id===)
+  }
+
   return (
     <div>
       <Header />
