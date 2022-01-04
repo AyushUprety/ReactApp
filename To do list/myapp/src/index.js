3a9d8655060f59eefb5e35d8406d33711ff68abe
AyushUprety <ayushuprety1@gmail.com>
2022-01-04 06:18:40 +0545

    Setup index.js to render root element

--- To do list/myapp/src/index.js
+++ To do list/myapp/src/index.js
@@ -1,3 +1,3 @@
-//  import ReactDom from 'react-dom';
+ import ReactDom from 'react-dom';
 
-//  ReactDom.render(<App/>,document.getElementById('root'))
+ ReactDom.render(<App/>,document.getElementById('root'))
