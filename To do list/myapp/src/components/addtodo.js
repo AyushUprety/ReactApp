fd9440604fee6a615d0c30f40127ec44613be22c
AyushUprety <ayushuprety1@gmail.com>
2022-01-04 17:42:02 +0545

    Added a function to listen to events that are generated when input value is changed

--- To do list/myapp/src/components/addtodo.js
+++ To do list/myapp/src/components/addtodo.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {useState} from "react"
 const Add = () => {
+  const[addItem,changeAddItem]=useState('')   
   return (
     <div>
-      <form method="" action="/">
-        <input type="text" />
+      <form method="" action="">
+        <input type="text"/>
         <button><i class="fas fa-plus-square"></i></button>
         <select>
             <option>All</option>
