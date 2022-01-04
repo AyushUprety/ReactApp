141f2067bacb6a900504ff9e1335552d71acc5dd
AyushUprety <ayushuprety1@gmail.com>
2022-01-04 07:25:56 +0545

    Added plus icon from font awesome to add new list and made a dropdown menu to sort out the list

--- To do list/myapp/src/components/addtodo.js
+++ To do list/myapp/src/components/addtodo.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
-const Add = ()=>{
-    return(
-        <div>
-            
-        </div>
-    )
-}
+import React from "react";
+const Add = () => {
+  return (
+    <div>
+      <form method="" action="/">
+        <input type="text" />
+        <button><i class="fas fa-plus-square"></i></button>
+        <select>
+            <option>All</option>
+            <option>Completed</option>
+            <option>uncompleted</option>
+        </select>
+      </form>
+    </div>
+  );
+};
+export default Add;
--- To do list/myapp/src/components/app.js
+++ To do list/myapp/src/components/app.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
+import Add from "./addtodo";
 
-const App = ()=>{
-    return (
-        <div>
-            <h1>Ayush's Todo List</h1>
-        </div>
-    )
-       
-    
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Ayush's Todo List</h1>
+      <Add/>
+    </div>
+  );
+};
 export default App;
