cc98e78143fb2ba3ec1eeda22bc30e4686ba2d30
AyushUprety <ayushuprety1@gmail.com>
2022-01-08 14:07:39 +0545

    created a new component to render todo list

--- To do list/myapp/src/components/addtodo.js
+++ To do list/myapp/src/components/addtodo.js
@@ -1,12 +1,21 @@
+//return nagari ni console la ta result dekhayo
+// aasti ko jasto anonymous function halnu parena
+
 import React from "react";
 import {useState} from "react"
 const Add = () => {
-  const[addItem,changeAddItem]=useState('')   
+  const[items,setItems]=useState([]);
+  const[userInput,setUserInput]=useState('');
   return (
     <div>
       <form method="" action="">
-        <input type="text"/>
-        <button><i class="fas fa-plus-square"></i></button>
+        <input type="text" onChange={e => setUserInput(e.target.value)}/> {/*use an anonymous function when you need to pass argument in the function that you are calling*/}
+        <button onClick={(e)=>{
+          e.preventDefault();
+          setItems(userInput);
+          console.log(items)
+          
+        }}><i className="fas fa-plus-square"></i></button>
         <select>
             <option>All</option>
             <option>Completed</option>
